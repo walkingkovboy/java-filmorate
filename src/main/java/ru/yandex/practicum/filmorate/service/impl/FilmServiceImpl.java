11456e6ded3f4d1ce042d40e3b953695b873b4fa
Александр Аникин <walkingkowboy@gmail.com>
2024-08-08 12:26:53 +0300

    рефакторинг кода

--- src/main/java/ru/yandex/practicum/filmorate/service/impl/FilmServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/impl/FilmServiceImpl.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;пше
+import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
