e7ffb2a9fb1ffebe731230d9a795ecfc73613506
Александр Аникин <walkingkowboy@gmail.com>
2024-08-13 05:01:45 +0300

    Рефакторинг кода

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -3,57 +3,30 @@ package ru.yandex.practicum.filmorate.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.NotExistException;
 
-@Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class ErrorHandler {
-    private final ObjectMapper objectMapper;
 
-    public ErrorHandler(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    public String handleValidationExceptions(MethodArgumentNotValidException ex) throws JsonProcessingException {
-        return errorToJson(ex.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+    public ErrorResponse handleFailValidation(final MethodArgumentNotValidException e) {
+        return new ErrorResponse(e.getMessage());
     }
 
-    @ExceptionHandler(NotExistException.class)
+    @ExceptionHandler({DataIntegrityViolationException.class, NotExistException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ResponseBody
-    public String handleNotFoundException(NotExistException ex) throws JsonProcessingException {
-        return errorToJson(ex.getMessage());
+    public ErrorResponse handleNotFound(final RuntimeException e) {
+        return new ErrorResponse(e.getMessage());
     }
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ResponseBody
-    public String handleOtherExceptions(Exception ex) throws JsonProcessingException {
-        return errorToJson(ex.getMessage());
-    }
-
-    private String errorToJson(String message) throws JsonProcessingException {
-        return objectMapper.writeValueAsString(new ErrorResponse(message));
-    }
-
-    static class ErrorResponse {
-        private final String error;
-
-        public ErrorResponse(String error) {
-            this.error = error;
-        }
-
-        public String getError() {
-            return error;
-        }
+    public ErrorResponse handleError(final Throwable e) {
+        return new ErrorResponse(e.getMessage());
     }
 }
