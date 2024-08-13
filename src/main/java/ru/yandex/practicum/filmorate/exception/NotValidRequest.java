709899c69bcd31bf903511b58709c6559bc77106
Александр Аникин <walkingkowboy@gmail.com>
2024-08-13 22:23:57 +0300

    Исправлены тесты

--- src/main/java/ru/yandex/practicum/filmorate/exception/NotValidRequest.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NotValidRequest.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class NotValidRequest extends RuntimeException{
+public class NotValidRequest extends RuntimeException {
     public NotValidRequest(String message) {
         super(message);
     }
