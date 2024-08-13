711e8af8585dd26b6a71679b49f3fc402cbe4a71
Александр Аникин <walkingkowboy@gmail.com>
2024-08-13 04:51:25 +0300

    Рефакторинг кода

--- src/main/java/ru/yandex/practicum/filmorate/service/RatingService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/RatingService.java
@@ -2,5 +2,5 @@ package ru.yandex.practicum.filmorate.service;
 
 import ru.yandex.practicum.filmorate.model.Rating;
 
-public interface RatingService extends BaseService<Rating>{
+public interface RatingService extends BaseService<Rating> {
 }
